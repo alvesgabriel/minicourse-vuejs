c915dda54f021d27a827fb28c37850e6cbef723c
Gabriel Alves <gabriel.alves.monteiro1@gmail.com>
2020-10-30 18:25:03 -0300

    :sparkles: Created To-do list with components
    close #11

---
+++ TODOTable.js
@@ -0,0 +1,32 @@
+Vue.component("todo-table", {
+  props: ["todos"],
+  data: function () {
+    return {};
+  },
+  methods: {
+    remove: function (todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+  },
+  template: `
+    <table class="table">
+      <thead>
+        <tr>
+            <th></th>
+            <th>To-do</th>
+            <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="todo in todos">
+            <td><input type="checkbox" v-model="todo.did"></td>
+            <td :class="{mark_did: todo.did}">{{todo.task}}</td>
+            <td>
+            <a @click="remove(todo)"><span class="icon has-text-info"><i class="fa fa-trash-o"></i></span></a>
+            </td>
+        </tr>
+      </tbody>
+    </table>
+  `,
+});
